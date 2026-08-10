158c85229498d59321c753604786b68f8f2fd763
mahamedalimohamedali-cpu <mahamedalimohamedali@gmail.com>
2026-08-11 01:35:02 +0300

    Add Tik_Tak_Toe/EnterNameForm.cs

---
+++ Tik_Tak_Toe/EnterNameForm.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tik_Tak_Toe
+{
+    public partial class EnterNameForm : Form
+    {
+        public EnterNameForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
